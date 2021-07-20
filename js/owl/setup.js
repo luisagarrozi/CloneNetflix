e75bd2f48798189dab999495e8625394d6e3c373
Luisa Oliveira <luisagarrozi@gmail.com>
2021-07-20 20:40:45 -0300

    PrimeiroCarrosel

--- js/owl/setup.js
+++ js/owl/setup.js
@@ -1,16 +1,29 @@
 $('.owl-carousel').owlCarousel({
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
     responsive:{
         0:{
-            items:1
+            items:2
         },
         600:{
             items:3
         },
+        800:{
+            items:4
+        },
         1000:{
-            items:5
+            items:6
         }
     }
 })
+
+const btnMenu = document.querySelector('[data-menu="button"]')
+const listMenu = document.querySelector('[data-menu="list"]')
+
+function abrirMenu(event){
+  listMenu.classList.toggle('active')
+  console.log("ok")
+}
+
+btnMenu.addEventListener('click', abrirMenu)
